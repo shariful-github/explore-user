c1f80362fa68220ec6b6d1a9a1f139f3f43b9471
sharif-github <107545661+sharif-github@users.noreply.github.com>
2024-02-11 19:48:25 +0600

    make responsiveness  404 page

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -10,9 +10,11 @@ const UserDetails = () => {
         return (
             <div className='flex flex-col justify-center items-center'>
                 <img src={notfound} alt="" />
-                <div role="alert" className="alert alert-error w-1/3">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                    <span>User Not Found to the Database</span>
+                <div className='mx-1 sm:mx-0'>
+                    <div role="alert" className="alert flex alert-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>User Not Found to the Database</span>
+                    </div>
                 </div>
             </div>
         )
